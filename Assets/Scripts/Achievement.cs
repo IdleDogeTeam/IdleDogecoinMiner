a52745fd2a241c596c6f61f8acbfb909c5564a6b
jeuxjeux20 <yaniaitamara2@yahoo.fr>
2016-08-31 16:31:16 +0200

    much better save manager now in beta (#3)
    * such revamped the save system
    
    * such better save manager
    
    wow

---
+++ Assets/Scripts/Achievement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Achievement : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+        Social.ShowAchievementsUI();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
