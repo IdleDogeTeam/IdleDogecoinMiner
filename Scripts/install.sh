0af13b5d6b995de9263fadc0535b6cabb16a8557
jeuxjeux20 <yaniaitamara2@yahoo.fr>
2016-09-01 22:14:29 +0200

    Much updated Travis unity with version build
    Such extended version

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -11,9 +11,9 @@
 
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 
-echo 'Downloading from http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: '
+echo 'Downloading from http://download.unity3d.com/download_unity/a6d8d714de6f/MacEditorInstaller/Unity-5.4.0f3.pkg: '
 
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
+curl -o Unity.pkg http://download.unity3d.com/download_unity/a6d8d714de6f/MacEditorInstaller/Unity-5.4.0f3.pkg
 
 
 
