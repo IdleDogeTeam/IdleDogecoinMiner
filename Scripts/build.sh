e63868c031dc3d5300e269354e8cbcc064af7ee2
OrangeChaten <josejoshua@hotmail.co.uk>
2016-09-01 20:30:08 +0100

    Very Travis

---
+++ Scripts/build.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+project="IdleDogeMiner"
+
+
+
+echo "Attempting to build $project for Windows"
+
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+
+  -batchmode \
+
+  -nographics \
+
+  -silent-crashes \
+
+  -logFile $(pwd)/unity.log \
+
+  -projectPath $(pwd) \
+
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+
+  -quit
+
+
+echo 'Logs from build'
+
+cat $(pwd)/unity.log
